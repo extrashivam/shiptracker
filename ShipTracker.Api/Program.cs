a319d538db1e4cb6f3aa9bfefe5c1b11eda983fb
Shivam Pathak <your-email@example.com>
2026-09-05 10:46:58 +0530

    Configure for Railway deployment

--- ShipTracker.Api/Program.cs
+++ ShipTracker.Api/Program.cs
@@ -12,13 +12,24 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("Postgres"))
        .UseSnakeCaseNamingConvention());
+
+var port = Environment.GetEnvironmentVariable("PORT");
+if (!string.IsNullOrEmpty(port))
+    builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+using (var scope = app.Services.CreateScope())
 {
-    app.MapOpenApi();
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
 }
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+    app.MapOpenApi();
+//}
 app.UseHttpsRedirection();
 app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "ShipTracker.Api v1"));
 
